de0c5819fb871823864831fe078930f6551d7168
kxxoling <kxxoling@gmail.com>
2016-07-29 17:59:26 +0800

    Get more emoticons from http://emoticon.moe/store/

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="jade">
 #app
-  .category(v-for="cate in emojiData.categories")
+  .category(v-for="cate in emoji.categories")
     .name
       | {{e.name}}
     .entries(v-for="e in cate.entries")
@@ -10,12 +10,22 @@
 
 <script>
 import Card from './components/Card';
-import emoji from '../data/kt.json';
+import ktc from '../data/ktc.json';
+import kt from '../data/kt.json';
+import jp from '../data/jp.json';
+import nimingban from '../data/nimingban.json';
+import plum from '../data/plum.json';
+import yashi from '../data/yashi.json';
+
+const emojiList = [
+  kt, ktc, plum, jp, nimingban, yashi,
+];
+const emoji = emojiList[0];
 
 export default {
   data() {
     return {
-      emojiData: emoji,
+      emoji,
     };
   },
   components: {
